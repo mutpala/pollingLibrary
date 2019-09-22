96facb730d7841eb9af0f8c12f220fb5f033c6e5
krishna9028 <krishna9028@gmail.com>
2019-09-22 13:28:29 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from "react";
+import {polling, pollTerminate} from "./polling";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState('');
+  let responseData = '';
+
+/* Polling Start */
+  function pollMethod() {
+    return axios.get("http://www.mocky.io/v2/5d857c78320000411007b26b");
+  }
+  
+  function afterPoll() {
+    responseData = responseData + JSON.stringify({'msg':'Polling Stopped....'});
+    setData(responseData);
+  }
+
+
+  let Obj = {
+    pollMethod: pollMethod,
+    pollInterval: 1000,
+    continuePolling: continuePolling,
+    onPollingStop: afterPoll,
+    logEnabled: true
+  };
+
+
+  function continuePolling(obj) {
+    if(obj.status === 200 && obj.data){
+      responseData = responseData + JSON.stringify(obj.data);
+      setData(responseData);
+      if (obj.data.nextAction === "tenderStart"){
+        return true;
+      } else{
+        return false;
+      }
+    }
+  }
+
+  const pollStart = () => {
+    if(polling({...Obj}) === 2) console.log('Duplicate Polling Start; ignored')
+  };
+
+/* Polling End */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <button onClick={pollStart}  > start Polling </button>
+      &nbsp;
+      <button onClick={pollTerminate}> stop Polling </button>
+      <br></br>
+      <div className="text"> {data} </div>
     </div>
   );
 }
---
+++ src/polling.js
@@ -0,0 +1,83 @@
+/*
+    -------------------A polling Utility---------------------
+    #########################################################
+    !!!!!!!!! Polling object will be passed as value !!!!!!!!
+    #########################################################
+    Please use helper functions to manipulate polling. 
+    Object should have below  fields
+    let Obj = {
+        pollMethod: pollMethod,                 // mandatory 
+        pollInterval: 1000,                     // mandatory
+        continuePolling: continuePolling,       // mandatory
+        onPollingStop: afterPoll,               // mandatory
+        logEnabled: true                        // optional
+    };
+*/
+
+let obj = {};
+
+const validateParms = obj => {
+    return obj.continuePolling &&
+      typeof obj.continuePolling === "function" &&
+      obj.hasOwnProperty("pollInterval") &&
+      (obj.pollMethod && typeof obj.pollMethod === "function")
+      ? true
+      : false;
+  };
+
+export const  polling = Obj => {
+  if(obj.hasPollingStarted){
+      return 2;
+  }
+  obj = {...Obj};
+  obj.hasPollingStarted = true;
+  const pollingFn = () => {
+    if (validateParms(obj)) {
+      obj.status = "IN_PROGRESS";
+      if(obj.logEnabled) console.log('pollMethod called');
+      const poll = obj.pollMethod();
+      poll.then(response => {
+        const ispollContinue = obj.continuePolling(response);
+        if (ispollContinue && !obj.isPollTerminate) {
+            if(obj.logEnabled) console.log('continuePolling condition met');
+            setTimeout(function() {
+            pollingFn();
+          }, obj.pollInterval);
+        } else {
+            if(obj.logEnabled && (obj.isPollTerminate && ispollContinue) ) console.log('poll terminated');
+            if(obj.logEnabled && (!obj.isPollTerminate && !ispollContinue) ) console.log('continuePolling condition not met');
+            obj.status = "IDLE";
+          return true;
+        }
+      });
+    } else {
+        if(obj.logEnabled) console.log('Invalid settings passed');
+        return false;
+    }
+  };
+  return pollingFn();
+};
+
+export const  pollingObj = () => {
+    return {...obj};
+}
+
+export const pollTerminate = () => {
+    if(obj.logEnabled) console.log('Terminate request initiated');
+    obj.isPollTerminate = true;
+    const waitForPoll = () => {
+        if (obj.status === "IDLE") {
+        obj.hasPollingStarted = false;
+        obj.onPollingStop();
+      } else {
+        if(obj.logEnabled) console.log('Terminate request - waiting');
+        setTimeout(waitForPoll, 10);
+      }
+    };
+    if(obj.hasPollingStarted){
+        waitForPoll();
+    }else{
+        if(obj.logEnabled) console.log('Polling Terminate is called before polling start');
+        return 2;
+    }
+  }
